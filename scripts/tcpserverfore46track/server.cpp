51de3dc7d9c3c1d4358f25d4b8ec62d876774786
Tamas Domok <domoktamas@gmail.com>
2020-03-09 21:50:33 +0100

    scripts/data: throttle m3.

--- scripts/tcpserverfore46track/server.cpp
+++ scripts/tcpserverfore46track/server.cpp
@@ -28,7 +28,6 @@ Server::handleReadyRead()
   qDebug() << "request" << buffer.toHex();
   const QByteArray request = buffer.mid(header.size(), buffer.size() - header.size() - 1);
 
-  static const QByteArray oil = QByteArray::fromHex("12050b031f");
   static const QByteArray dsc_steering = QByteArray::fromHex("b829f1032201f5b5");
   static const QByteArray dsc_brake = QByteArray::fromHex("b829f102210645");
 
@@ -38,9 +37,12 @@ Server::handleReadyRead()
   // latg -0.3:0.3 (-0.1:0.1)
   static const QByteArray dsc_offsets = QByteArray::fromHex("b829f102210241");
 
-  if (request == oil) {
+  static const QByteArray motronic_m3_throttle = QByteArray::fromHex("12050b031f");
+
+  if (request == motronic_m3_throttle) {
     qDebug() << "sending oil response";
-    socket->write(QByteArray::fromHex("122da002c30000000038003899bbb5a7c80188fc4e64359c79887c2f7c55fefefefe1447097605050d2e9086c2")); // oil temp 87.377
+    //socket->write(QByteArray::fromHex("1227a0000003b600000000000069858b65073a7b8181a60008000440043303e803f603e8635c73")); // throttle 100
+    socket->write(QByteArray::fromHex("1227a0000003b600000000000069858b65073a7c8181a600080001e201dd0223022f0223635ce0")); // throttle 54.7
   } else if (request == dsc_steering) {
     qDebug() << "sending dsc steering wheel response";
     socket->write(QByteArray::fromHex("b8f1290c6201f5010110dd8180810000b7"));  // angle 184.365
